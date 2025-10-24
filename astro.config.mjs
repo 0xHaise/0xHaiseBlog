5fc4b2d5049c5f45369877dc0382af83ca8c68ec
0xHaise <173845338+0xHaise@users.noreply.github.com>
2025-10-23 20:05:16 -0500

    update config files

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,8 +26,8 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://fuwari.vercel.app/",
-	base: "/",
+	site: 'https://0xhaise.github.io',
+	base: "/0xHaiseBlog",
 	trailingSlash: "always",
 	integrations: [
 		tailwind({
